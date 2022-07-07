8383ed30c7e421afef3ff4677c17ba1ba21a80eb
Jacqueline Jou <jacquelinejou@fb.com>
2022-07-07 16:37:50 -0700

    Connected Parse data to the map so that each data post shows up as a data point on the map.

---
+++ Capstone/Cells/PostCell.h
@@ -0,0 +1,17 @@
+//
+//  PostCell.h
+//  Capstone
+//
+//  Created by jacquelinejou on 7/7/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "Post.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostCell : UITableViewCell
+@property (nonatomic, strong) Post *post;
+@end
+
+NS_ASSUME_NONNULL_END
